7ef4467d7daf6196cf04545a6a265277a361d14b
Derek <Derek@Dereks.local>
2017-08-06 22:43:46 +0900

    remove unecessary .toString()

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 fswatch -o ./InstanceLocator.sketchplugin/ | while read num ;
 	do
-		echo num
+		echo "$num"
 		cp -R ./InstanceLocator.sketchplugin ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins/InstanceLocator.sketchplugin && echo "built"
 	done
