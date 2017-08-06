8b4812a14a958e8bdf1befb35ae6b95d0c8804a0
Derek <Derek@Dereks.local>
2017-08-06 22:40:30 +0900

    use fswatch

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-cp -R ./InstanceLocator.sketchplugin ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins/InstanceLocator.sketchplugin && echo "built"
+fswatch -o ./InstanceLocator.sketchplugin/ | while read num ;
+	do
+		echo num
+		cp -R ./InstanceLocator.sketchplugin ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins/InstanceLocator.sketchplugin && echo "built"
+	done
