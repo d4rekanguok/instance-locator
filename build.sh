c4c011008f787fefcf6d2f41eacd739905b3dc76
Derek <dikanguok@gmail.com>
2017-08-07 16:46:52 +0900

    modify build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+cp -R ./InstanceLocator.sketchplugin ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins/InstanceLocator.sketchplugin && echo "built"
+
 fswatch -o ./InstanceLocator.sketchplugin/ | while read num ;
 	do
 		echo "$num"
